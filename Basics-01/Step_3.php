466e958883ce97edd79efe13a6358749695cab08
Yahya <ydarwich18@gmail.com>
2022-02-24 14:47:53 +0200

    Step 4

--- Basics-01/Step_3.php
+++ Basics-01/Step_3.php
@@ -3,7 +3,8 @@ $name = "Yahya Darwich";
 $email = "add232344@gmail.com";
 echo "My name is $name and my email is $email.";
 $height = 12;
-$width = 34;
+$width = 54;
 $area = $height * $width;
 echo "The area equal $area";
+// use "$name" if you have text near variables or $name without quotes when don't have text, don't use '$name'
 ?>
---
+++ Basics-01/Step_4.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Yahya";
+$position = "Web Developper";
+$github_url = "https://github.com/YahyaDarwich";
+$var1 = "Hello, I'm ";
+$var2 = ", I'm a ";
+$var3 = " please check my github link ";
+$var4 = ".";
+echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url.".";
+echo "Hello, I'm $name, I'm a $position please check my github link $github_url.";
+echo $var1.$name.$var2.$position.$var3.$github_url.$var4;
+//Hello, I'm Gaby, I'm a tech consultant please check my github link https://github.com/gk-git.
+?>
