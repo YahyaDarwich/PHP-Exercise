7283e6a169a16ed76fe9fdd70ffbaeedd7fadd34
Yahya <ydarwich18@gmail.com>
2022-02-24 14:15:02 +0200

    Step 3

---
+++ Basics-01/Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Yahya Darwich";
+$email = "add232344@gmail.com";
+echo "My name is $name and my email is $email.";
+$height = 12;
+$width = 34;
+$area = $height * $width;
+echo "The area equal $area";
+?>
