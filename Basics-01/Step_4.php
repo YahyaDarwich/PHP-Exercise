d3e15cbed37a523b9673d2e18b4123afebaafeef
Yahya <ydarwich18@gmail.com>
2022-02-24 14:55:28 +0200

    Step 4

--- Basics-01/Step_4.php
+++ Basics-01/Step_4.php
@@ -6,8 +6,16 @@ $var1 = "Hello, I'm ";
 $var2 = ", I'm a ";
 $var3 = " please check my github link ";
 $var4 = ".";
+$var = "Hello, I'm ";
+$var .= $name;
+$var .= ", I'm a ";
+$var .= $position;
+$var .= " please check my github link ";
+$var .= $github_url;
+$var .= ".";
 echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url.".";
 echo "Hello, I'm $name, I'm a $position please check my github link $github_url.";
 echo $var1.$name.$var2.$position.$var3.$github_url.$var4;
+echo $var;
 //Hello, I'm Gaby, I'm a tech consultant please check my github link https://github.com/gk-git.
 ?>
