1c3003ec9da64bffe36065b503143f1795703ded
Yahya <ydarwich18@gmail.com>
2022-02-24 16:21:17 +0200

    Step 5

---
+++ Basics-01/Step_5.php
@@ -0,0 +1,10 @@
+<?php
+$course_name = "React";
+$enrolled_students = 17;
+$price = 3.66;
+$on_discount = true;
+echo "Course title: $course_name";
+echo " Enrolled Students: $enrolled_students";
+echo " Course price: $price $ USD";
+echo " Course on discount: $on_discount";
+?>
